bc074c0e2390d722d1d5619132a43a4e4362b4f6
Alexander Semenyuk <facemmannn403@gmail.com>
2021-11-10 23:21:41 +0300

    Jetpack Compose basics Codelab

--- app/src/main/java/com/github/justalexandeer/jetpackcomposesandbox/MainActivity.kt
+++ app/src/main/java/com/github/justalexandeer/jetpackcomposesandbox/MainActivity.kt
@@ -1,28 +1,29 @@
 package com.github.justalexandeer.jetpackcomposesandbox
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.Outline
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.material.ListItem
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import com.github.justalexandeer.jetpackcomposesandbox.ui.theme.JetpackComposeSandboxTheme
 
@@ -31,77 +32,127 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeSandboxTheme {
-                Conversation(
-                    mutableListOf<Message>(
-                        Message("Вася пупки", "ПРивет увидимся с тобой?"),
-                        Message(
-                            "Вася пупки", "тесттесттесттесттест \n" +
-                                    "тесттесттесттесттест" +
-                                    "тесттесттесттесттесттесттесттесттесттест"
-                        ),
-                        Message("Вася", "534523452435345"),
-                        Message("пупки", "вфапывари ываап ыв п"),
-                        Message(
-                            "Ваки",
-                            "пвыап       тесттесттесттесттест тесттесттесттесттест     фыав"
-                        ),
-
-                        )
-                )
+                MyApp()
             }
         }
     }
 
     @Composable
-    fun Conversation(messages: List<Message>) {
-        LazyColumn {
-            items(messages) { message ->
-                MessageCard(message)
+    private fun MyApp() {
+        var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+
+    @Composable
+    private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+            items(items = names) { name ->
+                Greeting(name = name)
             }
         }
     }
 
     @Composable
-    fun MessageCard(msg: Message) {
-        JetpackComposeSandboxTheme {
-            Row(modifier = Modifier.padding(all = 4.dp)) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "test",
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(CircleShape)
-                        .border(4.dp, MaterialTheme.colors.secondary, CircleShape)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                var isExpanded by remember { mutableStateOf(false) }
-                Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-                    Text(
-                        text = msg.author,
-                        color = MaterialTheme.colors.secondaryVariant,
-                        style = MaterialTheme.typography.subtitle2
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Surface(shape = MaterialTheme.shapes.medium, elevation = 4.dp) {
-                        Text(
-                            text = msg.body,
-                            modifier = Modifier.padding(all = 4.dp),
-                            maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                            style = MaterialTheme.typography.body2
-                        )
-                    }
+    fun OnboardingScreen(onContinueClicked: () -> Unit) {
+        Surface {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Welcome to the Basics Codelab!")
+                Button(
+                    modifier = Modifier.padding(vertical = 24.dp),
+                    onClick = onContinueClicked
+                ) {
+                    Text("Continue")
                 }
             }
         }
     }
 
-    @Preview
     @Composable
-    fun PreviewMessageCard() {
-        MessageCard(
-            Message("test", "test2")
-        )
+    private fun Greeting(name: String) {
+        var expanded by remember { mutableStateOf(false) }
+        Surface(
+            color = MaterialTheme.colors.primary,
+            modifier = Modifier
+                .padding(12.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+        ) {
+            Row(modifier = Modifier.padding(24.dp)) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(12.dp)
+                ) {
+                    Text(text = "Hello,")
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+                    )
+                    if (expanded) {
+                        Text(
+                            text = ("Composem ipsum color sit lazy, " +
+                                    "padding theme elit, sed do bouncy. ").repeat(4),
+                        )
+                    }
+                }
+                IconButton(onClick = { expanded = !expanded }) {
+                    Icon(
+                        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                        contentDescription = if (expanded) {
+                            stringResource(R.string.show_less)
+                        } else {
+                            stringResource(R.string.show_more)
+                        }
+
+                    )
+                }
+                /*
+                OutlinedButton(onClick = { expanded = !expanded }) {
+                    Text(if (expanded) "Show less" else "Show more")
+                }*/
+            }
+        }
+    }
+
+    @Preview(showBackground = true, widthDp = 420)
+    @Composable
+    fun DefaultPreview() {
+        JetpackComposeSandboxTheme {
+            Greetings()
+        }
+    }
+
+    @Preview(showBackground = true, widthDp = 320, heightDp = 320)
+    @Composable
+    fun GreetingPreview() {
+        JetpackComposeSandboxTheme {
+            OnboardingScreen(onContinueClicked = {})
+        }
+    }
+
+    @Preview(
+        showBackground = true,
+        widthDp = 320,
+        uiMode = UI_MODE_NIGHT_YES,
+        name = "DefaultPreviewDark"
+    )
+    @Preview(showBackground = true, widthDp = 320)
+    @Composable
+    fun DefaultPreviewDark() {
+        JetpackComposeSandboxTheme {
+            Greetings()
+        }
     }
 }
-
-data class Message(val author: String, val body: String)
