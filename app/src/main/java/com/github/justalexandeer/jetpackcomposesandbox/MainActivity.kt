33e1e94ff5456eb0c0e92dc648bec144233bd4f4
Alexander Semenyuk <facemmannn403@gmail.com>
2021-11-12 17:19:17 +0300

    Layouts in Jetpack Compost

--- app/src/main/java/com/github/justalexandeer/jetpackcomposesandbox/MainActivity.kt
+++ app/src/main/java/com/github/justalexandeer/jetpackcomposesandbox/MainActivity.kt
@@ -1,158 +1,164 @@
 package com.github.justalexandeer.jetpackcomposesandbox
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import android.graphics.Outline
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasureResult
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.github.justalexandeer.jetpackcomposesandbox.ui.theme.JetpackComposeSandboxTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeSandboxTheme {
-                MyApp()
+                LayoutsCodelab()
             }
         }
     }
 
-    @Composable
-    private fun MyApp() {
-        var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-        if (shouldShowOnboarding) {
-            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
-        } else {
-            Greetings()
-        }
-    }
+    val topics = listOf(
+        "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+        "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+        "Religion", "Social sciences", "Technology", "TV", "Writing"
+    )
 
     @Composable
-    private fun Greetings(names: List<String> = List(1000) { "$it" }) {
-        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-            items(items = names) { name ->
-                Greeting(name = name)
-            }
-        }
-    }
-
-    @Composable
-    fun OnboardingScreen(onContinueClicked: () -> Unit) {
-        Surface {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text("Welcome to the Basics Codelab!")
-                Button(
-                    modifier = Modifier.padding(vertical = 24.dp),
-                    onClick = onContinueClicked
-                ) {
-                    Text("Continue")
-                }
-            }
-        }
-    }
-
-    @Composable
-    private fun Greeting(name: String) {
-        var expanded by remember { mutableStateOf(false) }
-        Surface(
-            color = MaterialTheme.colors.primary,
-            modifier = Modifier
-                .padding(12.dp)
-                .animateContentSize(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioMediumBouncy,
-                        stiffness = Spring.StiffnessLow
-                    )
+    fun LayoutsCodelab() {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = "LayoutsCodelab")
+                    },
+                    actions = {
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(Icons.Filled.Favorite, contentDescription = null)
+                        }
+                    }
                 )
-        ) {
-            Row(modifier = Modifier.padding(24.dp)) {
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(12.dp)
-                ) {
-                    Text(text = "Hello,")
-                    Text(
-                        text = name,
-                        style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
-                    )
-                    if (expanded) {
-                        Text(
-                            text = ("Composem ipsum color sit lazy, " +
-                                    "padding theme elit, sed do bouncy. ").repeat(4),
-                        )
+            }
+        ) { innerPadding ->
+            BodyContent(Modifier.padding(innerPadding))
+        }
+    }
+
+    @Composable
+    fun BodyContent(modifier: Modifier = Modifier) {
+        Row(modifier = modifier
+            .background(color = Color.LightGray)
+            .padding(16.dp)
+            .size(200.dp)
+            .horizontalScroll(rememberScrollState()),
+            content = {
+                StaggeredGrid {
+                    for (topic in topics) {
+                        Chip(modifier = Modifier.padding(8.dp), text = topic)
                     }
                 }
-                IconButton(onClick = { expanded = !expanded }) {
-                    Icon(
-                        imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
-                        contentDescription = if (expanded) {
-                            stringResource(R.string.show_less)
-                        } else {
-                            stringResource(R.string.show_more)
-                        }
+            })
+    }
 
+    @Composable
+    fun StaggeredGrid(
+        modifier: Modifier = Modifier,
+        rows: Int = 3,
+        content: @Composable () -> Unit
+    ) {
+        Layout(
+            modifier = modifier,
+            content = content
+        ) { measurables, constraints ->
+
+            val rowWidths = IntArray(rows) { 0 }
+            val rowHeights = IntArray(rows) { 0 }
+
+            val placeables = measurables.mapIndexed { index, measurable ->
+                val placeable = measurable.measure(constraints)
+                val row = index % rows
+                rowWidths[row] += placeable.width
+                rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+                placeable
+            }
+
+            val width = rowWidths.maxOrNull()
+                ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+                ?: constraints.minWidth
+
+            val height = rowHeights.sumOf { it }
+                .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+            val rowY = IntArray(rows) { 0 }
+            for (i in 1 until rows) {
+                rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+            }
+
+            layout(width, height) {
+                val rowX = IntArray(rows) { 0 }
+                placeables.forEachIndexed { index, placeable ->
+                    val row = index % rows
+                    placeable.placeRelative(
+                        x = rowX[row],
+                        y = rowY[row]
                     )
+                    rowX[row] += placeable.width
                 }
-                /*
-                OutlinedButton(onClick = { expanded = !expanded }) {
-                    Text(if (expanded) "Show less" else "Show more")
-                }*/
             }
         }
     }
 
-    @Preview(showBackground = true, widthDp = 420)
     @Composable
-    fun DefaultPreview() {
-        JetpackComposeSandboxTheme {
-            Greetings()
+    fun Chip(modifier: Modifier = Modifier, text: String) {
+        Card(
+            modifier = modifier,
+            border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(16.dp, 16.dp)
+                        .background(color = MaterialTheme.colors.secondary)
+                )
+                Spacer(Modifier.width(4.dp))
+                Text(text = text)
+            }
         }
     }
 
-    @Preview(showBackground = true, widthDp = 320, heightDp = 320)
+    @Preview
     @Composable
-    fun GreetingPreview() {
+    fun ChipPreview() {
         JetpackComposeSandboxTheme {
-            OnboardingScreen(onContinueClicked = {})
+            Chip(text = "Hi there")
         }
     }
 
-    @Preview(
-        showBackground = true,
-        widthDp = 320,
-        uiMode = UI_MODE_NIGHT_YES,
-        name = "DefaultPreviewDark"
-    )
-    @Preview(showBackground = true, widthDp = 320)
+    @Preview
     @Composable
-    fun DefaultPreviewDark() {
+    fun LayoutsCodelabPreview() {
         JetpackComposeSandboxTheme {
-            Greetings()
+            LayoutsCodelab()
         }
     }
 }
